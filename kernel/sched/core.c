f459e30a4295433234616dc233347b3a2dd1f056
xperiafan13-rom <androidanimerom@gmail.com>
2017-09-21 17:31:23 -0500

    Update core

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3081,7 +3081,6 @@ unsigned long long task_delta_exec(struct task_struct *p)
 	unsigned long flags;
 	struct rq *rq;
 	u64 ns = 0;
-
 	rq = task_rq_lock(p, &flags);
 	ns = do_task_delta_exec(p, rq);
 	task_rq_unlock(rq, p, &flags);
