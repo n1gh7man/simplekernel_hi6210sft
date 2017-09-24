9aab4a77600d3259c4d86f448264a3edea7e29a2
kosmichak <kosmitchak@gmail.com>
2017-09-24 16:19:09 +0200

    Revert "Update drivers: wakeup"
    This reverts commit 489d2df01f83fc7108579a32f41c6ff789f1768a.

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -606,7 +606,6 @@ static int device_resume_early(struct device *dev, pm_message_t state)
 static void dpm_resume_early(pm_message_t state)
 {
 	ktime_t starttime = ktime_get();
-	pm_print_active_wakeup_sources();
 
 	mutex_lock(&dpm_list_mtx);
 	while (!list_empty(&dpm_late_early_list)) {
