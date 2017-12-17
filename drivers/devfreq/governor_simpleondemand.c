bc42244e41e3060288b8e37165694a04137ac37d
kosmichak <kosmitchak@gmail.com>
2017-12-17 15:00:25 +0100

    ignore

--- drivers/devfreq/governor_simpleondemand.c
+++ drivers/devfreq/governor_simpleondemand.c
@@ -29,6 +29,7 @@ static int devfreq_simple_ondemand_func(struct devfreq *df,
 	unsigned int dfso_downdifferential = DFSO_DOWNDIFFERENCTIAL;
 	struct devfreq_simple_ondemand_data *data = df->data;
 	unsigned long max = (df->max_freq) ? df->max_freq : UINT_MAX;
+	unsigned long min = (df->min_freq) ? df->min_freq : 0;
 
 	if (err)
 		return err;
@@ -49,7 +50,7 @@ static int devfreq_simple_ondemand_func(struct devfreq *df,
 		stat.total_time >>= 7;
 	}
 
-	if (data && data->simple_scaling) {
+	if (data) {
 		if (stat.busy_time * 100 >
 		    stat.total_time * dfso_upthreshold)
 			*freq = max;
@@ -66,12 +67,6 @@ static int devfreq_simple_ondemand_func(struct devfreq *df,
 	if (stat.total_time == 0) {
 		*freq = max;
 		return 0;
-	}
-
-	/* Prevent overflow */
-	if (stat.busy_time >= (1 << 24) || stat.total_time >= (1 << 24)) {
-		stat.busy_time >>= 7;
-		stat.total_time >>= 7;
 	}
 
 	/* Set MAX if it's busy enough */
